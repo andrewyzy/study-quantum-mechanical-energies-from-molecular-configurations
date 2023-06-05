287682a70b4f8f15c05538ba20add993ad76a14d
André Brasil Vieira Wyzykowski <37346636+andrewyzy@users.noreply.github.com>
2023-06-05 17:29:16 -0400

    Add files via upload
    Fixing some bugs and add energy distribution probabilistic

--- energy_molecule_simulation.py
+++ energy_molecule_simulation.py
@@ -31,65 +31,42 @@ class Molecule:
             raise ValueError('Invalid molecule type')
 
     def absorb_light(self, energy, temperature):
-        # Apply the Arrhenius equation to adjust the max energy level based on the temperature
-        R = 8.314  # Universal gas constant in J/(mol*K)
-        Ea = self.activation_energy  # Activation energy in J/mol, specific to the type of molecule
-        A = self.max_energy_level  # We'll just use the max_energy_level as the pre-exponential factor for simplicity
-        adjusted_max_energy_level = A * math.exp(-Ea / (R * temperature))
+        R = 8.314
+        Ea = self.activation_energy
+        A = self.max_energy_level
+        adjusted_max_energy_level = A * math.exp(-Ea / (100 * R * temperature))
+        print("adjusted_max_energy_level = ", adjusted_max_energy_level)
 
-        # Adjust the absorbed energy based on the heat capacity
-        absorbed_energy = energy / self.heat_capacity  # This is a simplification - in reality the relationship may not be linear
+
+        # Add a random factor to energy absorption
+        absorbed_energy = random.uniform(0, energy / self.heat_capacity)
+        print(f"Absorbed energy: {absorbed_energy}")
+
 
         potential_energy_level = self.current_energy_level + absorbed_energy
-        if potential_energy_level <= adjusted_max_energy_level:
-            self.current_energy_level = potential_energy_level
-            print(f"Molecule at position {self.position} has moved to energy level {self.current_energy_level}")
-            return 0  # no excess energy
+        print(f"Potential energy level before rounding: {potential_energy_level}")
+
+
+        # Add a condition to check if the potential energy level is too high
+        if potential_energy_level > adjusted_max_energy_level:
+            absorbed_energy = adjusted_max_energy_level - self.current_energy_level
+            potential_energy_level = self.current_energy_level + absorbed_energy
+            return 0
         else:
-            excess_energy = potential_energy_level - self.max_energy_level
-            self.current_energy_level = self.max_energy_level
-            print(f"Molecule at position {self.position} has moved to max energy level {self.max_energy_level}")
-            # Adjust the excess energy based on the number of molecules in a given volume
-            volume = 1  # For simplicity, let's assume a volume of 1 m3
-            number_of_molecules = self.density * volume / self.molar_mass  # This is a rough estimate - in reality we'd need to use the Ideal Gas Law or another model to calculate this
-            adjusted_excess_energy = excess_energy / number_of_molecules  # Again, this is a simplification - in reality the relationship may not be linear
-            return adjusted_excess_energy  # return the adjusted excess energy
+            excess_energy = potential_energy_level - adjusted_max_energy_level
+            self.current_energy_level = adjusted_max_energy_level
+            print(f"Molecule at position {self.position} has moved to max energy level {adjusted_max_energy_level}")
 
-
-    def absorb_light(self, energy, temperature):
-        # Apply the Arrhenius equation to adjust the max energy level based on the temperature
-        R = 8.314  # Universal gas constant in J/(mol*K)
-        Ea = self.activation_energy  # Activation energy in J/mol, specific to the type of molecule
-        A = self.max_energy_level  # We'll just use the max_energy_level as the pre-exponential factor for simplicity
-        adjusted_max_energy_level = A * math.exp(-Ea / (R * temperature))
-
-        # Adjust the absorbed energy based on the heat capacity
-               # Adjust the absorbed energy based on the heat capacity
-        absorbed_energy = energy / self.heat_capacity
-        print(f"Absorbed energy: {absorbed_energy}")  # Add this line
-
-        potential_energy_level = self.current_energy_level + absorbed_energy
-        print(f"Potential energy level before rounding: {potential_energy_level}")  # Add this line
-
-        # Round to the nearest multiple of h
-        potential_energy_level = round(potential_energy_level / self.h) * self.h
-
-        if potential_energy_level <= adjusted_max_energy_level:
-            self.current_energy_level = potential_energy_level
-            print(f"Molecule at position {self.position} has moved to energy level {self.current_energy_level}")
-            return 0  # no excess energy
-        else:
-            excess_energy = potential_energy_level - self.max_energy_level
-            self.current_energy_level = self.max_energy_level
-            print(f"Molecule at position {self.position} has moved to max energy level {self.max_energy_level}")
-            volume = 1  # For simplicity, let's assume a volume of 1 m3
-            number_of_molecules = self.density * volume / self.molar_mass  # This is a rough estimate - in reality we'd need to use the Ideal Gas Law or another model to calculate this
-            adjusted_excess_energy = excess_energy / number_of_molecules  # Again, this is a simplification - in reality the relationship may not be linear
-            return adjusted_excess_energy  # return the adjusted excess energy
+            volume = 1
+            number_of_molecules = self.density * volume / self.molar_mass
+            adjusted_excess_energy = excess_energy / number_of_molecules
+            return adjusted_excess_energy
 
 
     def transfer_energy(self, energy):
         potential_energy_level = self.current_energy_level + energy
+        print("potential_energy_level = ", potential_energy_level)
+
         if potential_energy_level <= self.max_energy_level:
             self.current_energy_level = potential_energy_level
             print(f"Molecule at position {self.position} has received energy, now at level {self.current_energy_level}")
@@ -106,7 +83,9 @@ class System:
 
     def pulse_light(self, energy):
         for molecule in self.molecules:
-            excess_energy = molecule.absorb_light(energy + self.temperature, self.temperature)
+            # Make energy distribution probabilistic
+            distributed_energy = random.uniform(0, energy)
+            excess_energy = molecule.absorb_light(distributed_energy + self.temperature, self.temperature)
             if excess_energy > 0:
                 # Find the nearest molecule to transfer energy to
                 distances = [self.distance(molecule.position, other_molecule.position) for other_molecule in self.molecules if other_molecule != molecule]
@@ -126,7 +105,7 @@ class System:
         x2, y2, z2 = position2
         return math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
 
-system = System(temperature=2)
+system = System(temperature=200)
 system.add_molecule(Molecule(5, (0, 0, 0), [1, 0, 0], 'CH4'))  # A methane molecule
 system.add_molecule(Molecule(5, (1, 1, 1), [0, 1, 0], 'C2H6'))  # An ethane molecule
 system.add_molecule(Molecule(5, (2, 2, 2), [0, 0, 1], 'C3H8'))  # A propane molecule
